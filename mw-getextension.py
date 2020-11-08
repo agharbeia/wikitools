a1a026d79625cc80b63708ff2366abfd1b12fe1e
Ahmad Gharbeia <gharbeia@gmail.com>
2020-11-08 23:34:03 +0000

    mentioned GPL 3.0 as the license + change order of arguments

--- mw-getextension.py
+++ mw-getextension.py
@@ -31,19 +31,19 @@ def main(args):
 		os.system('wget --continue' + (' --output-document - ' if not args.no_extract else ' --directory-prefix={} '.format(args.target_dir)) + bundle_URL + ('' if args.no_extract else ' | tar --directory={} --extract --file=- --gunzip'.format(args.target_dir)) + ' && mv {0}/{1} {0}/{2}'.format(args.target_dir, extension, match.group('bundle')))
 ## end main
 
-ap = argparse.ArgumentParser(description=help_text)
+ap = argparse.ArgumentParser(description=help_text, epilog='By Ahmad Gharbeia, November 2020; Under GPL 3.0 license')
 
 ap.add_argument('extension', nargs='+',
 		help='Name of the MW extension.')
 
 ap.add_argument('--mw-version', action='store', default=MW_VER,
 	help='The major release version of Mediawiki to get extensions for. It currently defults to ' + MW_VER)
-                    
-ap.add_argument('--no-extract', action='store_true', default=False,
-  help='Download the extension bundle but do not extract it.')
 
 ap.add_argument('--target-dir', action='store', default='./',
 	help='The name of the directory to save bundle or extract it to.')
+                    
+ap.add_argument('--no-extract', action='store_true', default=False,
+  help='Download the extension bundle but do not extract it.')
 
 ap.add_argument('--verbose', action='store_true', default=False,
 	help='Output more details about what\'s happening.')
