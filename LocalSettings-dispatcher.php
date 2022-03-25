fe8972f6c4c357e696d899a4bc6c1f5dceb6b666
Ahmad Gharbeia <gharbeia@gmail.com>
2022-03-25 11:57:08 +0000

    cosmetic

--- LocalSettings-dispatcher.php
+++ LocalSettings-dispatcher.php
@@ -11,22 +11,22 @@ if ( !defined( 'MEDIAWIKI' ) ) {
 $WebRoot = "/srv/www/wikido.xyz/";
 
 if ( defined( 'MW_DB' ) ) {
-    // Command-line mode and maintenance scripts (e.g. update.php) 
-    $wikiname = substr(MW_DB, 0, -5);
-    require_once ($WebRoot . $wikiname . "/LocalSettings.php");
+	// Command-line mode and maintenance scripts (e.g. update.php) 
+	$wikiname = substr(MW_DB, 0, -5);
+	require_once ($WebRoot . $wikiname . "/LocalSettings.php");
 } else {
-    // Assume it being a web request
+// Assume it being a web request
 	if ( array_key_exists('SERVER_NAME', $_SERVER) ) {
-	    $server = $_SERVER['SERVER_NAME'];
-	    if ( preg_match( '/^(.*)\.wikido.xyz$/', $server, $matches ) ) {
-    	    $wikiname = $matches[1];
-	    } else {
-    	    die( "We should not be here since Apache shouldn't have routed a request to $server to WikiDo." );
-    	}
+		$server = $_SERVER['SERVER_NAME'];
+		if ( preg_match( '/^(.*)\.wikido.xyz$/', $server, $matches ) ) {
+			$wikiname = $matches[1];
+		} else {
+			die( "We should not be here since Apache shouldn't have routed a request to $server to WikiDo." );
+		}
 	} else {
 		die("Couldn't guess which WikiDo member to work on.");
 	}
-    require_once ($WebRoot . $wikiname . "/LocalSettings.php");
+	require_once ($WebRoot . $wikiname . "/LocalSettings.php");
 }
 
 ## Global directives
